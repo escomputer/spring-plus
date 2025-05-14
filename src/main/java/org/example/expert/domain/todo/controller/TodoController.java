af2aae5a5c45d00440123f924c118fcf518f9e2d
escomputer <nujest307@naver.com>
2025-05-15 01:50:30 +0900

    No.9-2 spring security ->authenticationprincipal 어노테이션 사용

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -3,7 +3,9 @@ package org.example.expert.domain.todo.controller;
 import java.time.LocalDate;
 
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
+
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
@@ -13,35 +15,36 @@ import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
 
-    private final TodoService todoService;
+	private final TodoService todoService;
 
-    @PostMapping("/todos")
-    public ResponseEntity<TodoSaveResponse> saveTodo(
-            @Auth AuthUser authUser,
-            @Valid @RequestBody TodoSaveRequest todoSaveRequest
-    ) {
-        return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
-    }
+	@PostMapping("/todos")
+	public ResponseEntity<TodoSaveResponse> saveTodo(
+		@AuthenticationPrincipal AuthUser authUser,
+		@Valid @RequestBody TodoSaveRequest todoSaveRequest
+	) {
+		return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
+	}
 
-    @GetMapping("/todos")
-    public ResponseEntity<Page<TodoResponse>> getTodos(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size,
-        @RequestParam(required = false) String weather,
-        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate startDate,
-        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size,weather,startDate,endDate));
-    }
+	@GetMapping("/todos")
+	public ResponseEntity<Page<TodoResponse>> getTodos(
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(required = false) String weather,
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+	) {
+		return ResponseEntity.ok(todoService.getTodos(page, size, weather, startDate, endDate));
+	}
 
-    @GetMapping("/todos/{todoId}")
-    public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
-        return ResponseEntity.ok(todoService.getTodo(todoId));
-    }
+	@GetMapping("/todos/{todoId}")
+	public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
+		return ResponseEntity.ok(todoService.getTodo(todoId));
+	}
 }
