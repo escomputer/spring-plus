b098c6abad4bbb0f6498f47a94dac926096871f1
escomputer <nujest307@naver.com>
2025-05-15 03:16:33 +0900

    Fix : query dsl 을 위한, 빈 등록

--- src/main/java/org/example/expert/config/PersistenceConfig.java
+++ src/main/java/org/example/expert/config/PersistenceConfig.java
@@ -1,9 +1,22 @@
 package org.example.expert.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Configuration
 @EnableJpaAuditing
 public class PersistenceConfig {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(){
+		return new JPAQueryFactory(em);
+	}
 }
