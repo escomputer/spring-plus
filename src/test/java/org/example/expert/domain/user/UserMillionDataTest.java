c986e32905d53cc1b58bae605a0ff7979a9960b6
escomputer <nujest307@naver.com>
2025-05-15 14:47:17 +0900

    no. 13 암호화 한 비밀번호로 유정생성

--- src/test/java/org/example/expert/domain/user/UserMillionDataTest.java
+++ src/test/java/org/example/expert/domain/user/UserMillionDataTest.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
+import org.example.expert.config.PasswordEncoder;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.example.expert.domain.user.repository.UserRepository;
@@ -29,30 +30,34 @@ public class UserMillionDataTest {
 	@Autowired
 	private EntityManager em;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Test
 	@Transactional
 	@Rollback(value = false)
-	void 유저_100만명_생성(){
+	void 유저_100만명_생성() {
 		List<User> users = new ArrayList<>();
 
-		for (int i=1; i<=1000000; i++) {
-			String nickname = "user_"+ UUID.randomUUID().toString();
-			String email = "user_"+ i +"@example.com";
-			String password = "password" + i;
+		String rawPassword = "password";
+		//패스워드 암호화 진행시킴 무거운 암호화 알고리즘 돌리니깐 반복문 내에 있으면, 병목현상 발생
+		String encodedPassword = passwordEncoder.encode(rawPassword);
 
+		for (int i = 1; i <= 1000000; i++) {
+			String nickname = "user_" + UUID.randomUUID().toString();
+			String email = "user_" + i + "@example.com";
 
-			User user = new User(email,password, UserRole.USER);
+			User user = new User(email, encodedPassword, UserRole.USER);
 			//private인 user에 직접 닉네임 할당
 			ReflectionTestUtils.setField(user, "nickname", nickname);
 
-
 			users.add(user);
-			if(i%10000==0){
+			if (i % 10000 == 0) {
 				userRepository.saveAll(users);
 				em.flush();
 				em.clear();
 				users.clear();
-				System.out.println(i+"명 생성 완료");
+				System.out.println(i + "명 생성 완료");
 			}
 		}
 
